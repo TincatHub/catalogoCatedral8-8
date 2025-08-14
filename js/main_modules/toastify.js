b73fd4600c3d36e7fd24c91509878647bc6d1900
TincatHub <martinanblanes1@gmail.com>
2025-08-13 21:07:32 -0300

    Delete js directory

--- js/main_modules/toastify.js
+++
@@ -1,8 +0,0 @@
-export const toastify = (status) => {
-    Toastify({
-        text: `Producto ${status}`,
-        duration: 3000,
-        close: true,
-        className: "toastifyToast"
-    }).showToast();
-}
